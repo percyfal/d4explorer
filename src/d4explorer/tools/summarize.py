0bf21840176226ed83a7efdf99ff98509f8d2c7e
Per Unneberg <per.unneberg@scilifelab.se>
2025-03-04 13:50:36 +0100

    Update summarize to annotate regions

--- src/d4explorer/tools/summarize.py
+++ src/d4explorer/tools/summarize.py
@@ -2,6 +2,7 @@
 
 import subprocess as sp
 import sys
+from collections import namedtuple
 
 import click
 import daiquiri
@@ -24,7 +25,7 @@ def log_level(expose_value=False):
             logger = daiquiri.getLogger("root")
             logger.setLevel(value)
         else:
-            loggers = ["d4explorer-d4filter"]
+            loggers = ["d4explorer-summarize"]
             for logname in loggers:
                 logger = daiquiri.getLogger(logname)
                 logger.setLevel(value)
@@ -63,12 +64,14 @@ def cli():
     default=sys.stdout,
     type=click.File("w"),
 )
+@log_level()
 def group(path, regions, threshold, output_file):
     """Summarize coverage data over regions.
 
     The input D4 file should consist of five columns (strand is
     excluded). Requires bedtools to run.
     """
+    row = namedtuple("row", ["seqid", "begin", "end", "score"])
 
     def summarize_region(cov, chrom):
         df = pd.DataFrame(
@@ -95,42 +98,45 @@ def group(path, regions, threshold, output_file):
         data = fh.readline().strip().split("\t")
         if len(data) != 4:
             raise ValueError("Region file must have four columns")
+    features = pd.read_table(regions, sep="\t", header=None)
+    features.columns = ["seqid", "start", "end", "name"]
     last = None
     first = True
     cov = []
+    d4view_cmd = ["d4tools", "view", "-R", regions, path]
     d4view = sp.Popen(
-        ["d4tools", "view", "-R", regions, path],
+        d4view_cmd,
         stdout=sp.PIPE,
         bufsize=1,
         universal_newlines=True,
     )
-    intersect = sp.Popen(
-        ["bedtools", "intersect", "-a", "-", "-b", regions, "-wb"],
-        stdin=d4view.stdout,
-        stdout=sp.PIPE,
-        bufsize=1,
-        universal_newlines=True,
-    )
-    cut = sp.Popen(
-        ["cut", "-f", "1,2,3,4,8"],
-        stdin=intersect.stdout,
-        stdout=sp.PIPE,
-        bufsize=1,
-        universal_newlines=True,
-    )
+    findex = 0
+    current_feature = features.iloc[findex]
+    i = 0
     while True:
-        line = cut.stdout.readline().strip()
-        if line == "" and cut.poll() is not None:
+        line = d4view.stdout.readline().strip()
+        i = i + 1
+        if line == "" and d4view.poll() is not None:
             break
-        if line:
-            rname = line.strip().split("\t")[0]
-            if first:
-                last = rname
-                first = False
-        if rname != last:
+        data = row(*line.strip().split("\t"))
+        if first:
+            last = data.seqid
+            first = False
+        if data.seqid != last:
             summarize_region(cov, last)
-            last = rname
+            last = data.seqid
             cov = []
-        else:
-            cov.append(line.strip().split("\t"))
+        if data.seqid != current_feature["seqid"]:
+            findex = findex + 1
+            current_feature = features.iloc[findex]
+        cov.append(list(data) + [current_feature["name"]])
+
+        if int(data.end) == current_feature["end"]:
+            findex = findex + 1
+            try:
+                current_feature = features.iloc[findex]
+            except IndexError:
+                # last row
+                pass
+
     summarize_region(cov, last)
